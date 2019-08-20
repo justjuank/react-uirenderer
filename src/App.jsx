bd3a6c4d6a7dedc6b2bea7ed9c20b4ea293a1e8e
Juan Charria <jcharria@fileandserve.com>
2019-08-20 16:39:08 -0500

    +Added a simple schema editor for the demo

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import './App.css';
 
 import UIRenderer from './components/UIRenderer';
 
+import JSONEditor from './demo/JSONEditor';
+
 import schema from './schemas/example1.json';
 
 const initialData = {
@@ -13,6 +15,9 @@ const initialData = {
 function App() {
   const [data, setData] = useState(initialData);
   const [valid, setValid] = useState(false);
+  const [uiSchema, setUiSchema] = useState(schema);
+  const [showDataOutput, setShowDataOutput] = useState(true);
+  const [showSchema, setShowSchema] = useState(true);
 
   const OnChange = useCallback((newData, newValid) => {
     setData(newData);
@@ -20,16 +25,25 @@ function App() {
     //console.log('Output', newData);
   }, []);
 
+  const OnSchemaModified = useCallback((newSchema) => {
+    setUiSchema(newSchema);
+  }, []);
+
   return (
     <div className="App">
       <h2>Dynamic UI Form</h2>
+      <p>Form Valid State: <b style={{color: valid===true ? '#0fc10f' : 'red'}}>{valid===true ? 'true' : 'false'}</b></p>
       <div style={{ float: 'left', width: '50%' }}>
-        <UIRenderer schema={schema} data={initialData} OnChange={OnChange} />
+        <UIRenderer schema={uiSchema} data={initialData} OnChange={OnChange} />
       </div>
       <div style={{ float: 'left', width: '45%', padding: '0 1rem' }}>
-        <h4>Data Output</h4>
-        <p>Form Valid State: {valid===true ? 'true' : 'false'}</p>
-        <pre style={{ border: '1px solid #ccc', padding: '1rem', minHeight: '300px' }}>{JSON.stringify(data, null, 2)}</pre>
+        <h4 style={{cursor: 'pointer'}} onClick={() => setShowDataOutput(!showDataOutput)}>Data Output</h4>
+        {(showDataOutput && <pre style={{ border: '1px solid #ccc', padding: '1rem', minHeight: '300px' }}>{JSON.stringify(data, null, 2)}</pre> )}
+
+        <div>
+        <h4 style={{cursor: 'pointer'}} onClick={() => setShowSchema(!showSchema)}>UI Schema <small>(Try changing it)</small></h4>
+          {(showSchema && <JSONEditor json={uiSchema} OnChange={OnSchemaModified}></JSONEditor> )}
+        </div>
       </div>
     </div>
   );
